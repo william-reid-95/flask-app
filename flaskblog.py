69cd619fa6203f3020b0441ace57ff6449e7667d
William Reid <54757743+william-reid-95@users.noreply.github.com>
2022-09-17 17:56:28 +1000

    new content
    - added html templates
    - added bootstrap css classes
    - added login and register pages + forms (wtforms)
    -added dummy login creds for testing bootstrap flash messages

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+#secret key is important for security, need to research further, but it encrypts cookiesd andotehr data.
+app.config['SECRET_KEY'] = '015182c115182aa5cc7d6303919cb86d'
+
+posts = [
+    {
+    "author":"Bob Fred",
+    "title":"Blog Post 1",
+    "content":"first post content",
+    "date_posted":"April 1, 2020"
+    },
+    {
+    "author":"Jane Doe",
+    "title":"Blog Post 2",
+    "content":"second post content",
+    "date_posted":"July 22, 2022"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts) #render templates must be stored in a folder called 'templates'
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title="about")
 
+@app.route("/register",methods=["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == "password":
+            flash('Successfully logged in!', "success")
+            return redirect(url_for('home'))
+        else:
+            flash("login failed, please check your account information and try again.", "danger")
+
+    return render_template('login.html',title='Login',form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+# converts python classes into html forms
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email,EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),Length(2,20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
