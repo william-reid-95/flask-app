ea3767657424fe50af8aa54ee9864ed74a20f2c8
William Reid <54757743+william-reid-95@users.noreply.github.com>
2022-09-17 20:04:02 +1000

    implemented sqlite DB
    used sql alchemy to add db

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,42 @@
+from email.mime import image
+from email.policy import default
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 
 #secret key is important for security, need to research further, but it encrypts cookiesd andotehr data.
 app.config['SECRET_KEY'] = '015182c115182aa5cc7d6303919cb86d'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app) #instance of database
+
+#classes associatef with db are reffered to as 'models'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable = False)
+    email = db.Column(db.String(120), unique=True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default='default.jpg') #will be hashed to 20 characters
+    password = db.Column(db.String(60), nullable = False) #will be hashed to 60 characters
+
+    posts = db.relationship('Post', backref="author", lazy=True)
+    
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable = False)
+    date_posted = db.Column(db.DateTime,nullable = False, default=datetime.utcnow) #default is equal to the function date.utcnmow, rather than the return value of that function
+    content = db.Column(db.Text,nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+
+
 
 posts = [
     {
